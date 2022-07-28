e18f7a2f31402360fcf1e6ea13e9dc0acb5f0d68
manas-vanblaq <manas.prabhutendolkar@anu.edu.in>
2022-07-28 08:06:48 +0530

    Created game window class for populating game

---
+++ JS/gameControl.js
@@ -0,0 +1,10 @@
+class GameWindow {
+    constructor(gameWidth, gameHeight){
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        this.states = [];
+        this.currentState = this.states[0];
+        this.bgImage = 
+
+    }
+}
