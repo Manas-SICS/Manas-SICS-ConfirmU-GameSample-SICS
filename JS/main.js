5e7d5147388fbb45091ccf178c6370cd920dc568
manas-vanblaq <manas.prabhutendolkar@anu.edu.in>
2022-07-29 16:48:16 +0530

    canvas setup

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){
+    const canvas = this.document.getElementById('demoGame');
+    const ctx = canvas.getContext ('2d');
+    canvas.width = 720;
+    canvas.height = 512;
+    
+})
