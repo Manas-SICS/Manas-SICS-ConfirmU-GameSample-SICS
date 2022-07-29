05440327a575922225b326356d900cf684848f4a
manas-vanblaq <manas.prabhutendolkar@anu.edu.in>
2022-07-29 16:50:12 +0530

    setup game class

--- JS/main.js
+++ JS/main.js
@@ -3,5 +3,20 @@ window.addEventListener('load', function(){
     const ctx = canvas.getContext ('2d');
     canvas.width = 720;
     canvas.height = 512;
-    
+
+
+    class Game {
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+        }
+
+        update(){
+
+        }
+
+        draw(){
+            
+        }
+    }
 })
